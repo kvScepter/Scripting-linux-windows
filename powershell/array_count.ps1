c23b075250fa44c24cf2d3b0c24810a08d22aec1
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 09:21:48 +0300

    Create array_count.ps1
    Counts objects in array and shows  what object is 1 and last

---
+++ powershell/array_count.ps1
@@ -0,0 +1,4 @@
+$names = "Kevin", "Harry", "Marv", "Frank", "Mr Andersson", "Peter", "Joe"
+Write-host "There are", $names.Count, "brothers"
+$names
+Write-Host "First name in the array is:"$names[0]", and the last name in the array is"$names[6]
