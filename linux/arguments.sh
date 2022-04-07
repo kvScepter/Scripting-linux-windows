9d7596b02e8a1e96dea59ea7810747b6350a24be
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 08:31:39 +0300

    Create arguments.sh

---
+++ linux/arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+A=$(( 5+7-2 ))
+B=$(( A * 10 ))
+
+echo $A 'is A argument and' $B 'is B argument' 
+echo 'Script contains two arguments and returns addition, substraction and multiplication'
