d51ee41e71d5f2c53b61548931caf0a7ca328d68
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 09:15:24 +0300

    Create hello.ps1
    ask´s user some information and prints them out

---
+++ powershell/hello.ps1
@@ -0,0 +1,4 @@
+$fname = read-host "Please, give me your firstname"
+$lname = Read-Host "Please, give me your lastname "
+$gcode = Read-Host "Please, give me your groupcode"
+Write-Host "welcome to course", $fname, $lname, $gcode
